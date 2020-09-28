031012c9cf4e4d24e97ee684a5df7508b6d0fcdf
fachidiot <jerrykim428@gmail.com>
2020-09-28 18:04:33 +0900

    .

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float f_FlySpeed;
+    public float f_Gravity;
+    
+    private Vector3 m_Movement;
+    private float m_fX;
+    private float m_fY;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            gameObject.GetComponent<Rigidbody>().AddForce(0, 300, 0);
+        }
+    }
+}
